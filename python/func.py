ceb932253b2c3e1bfafad0b359b64a2594cf1e80
Lysandre <lysandre.debut@reseau.eseo.fr>
2018-10-22 20:48:22 +0200

    Implemented new getxn in Python

--- python/func.py
+++ python/func.py
@@ -3,36 +3,166 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.io import wavfile
 import os
+from sklearn.decomposition import FastICA, PCA
+import time
+import json
+from syllabe import get_basic_signals, get_signal, get_entire_signal
 dirpath = os.path.dirname(os.path.abspath('__file__'))
 
 def sine(frequency, size=1000):
     return np.sin(np.linspace(0, frequency, size))
 
+
 def sign(frequency, size=1000):
     return np.sign(sine(frequency, size))
+
 
 def chirp(frequency, size=1000):
     return signal.chirp(np.linspace(0, 10, size), f0=frequency[0], f1=frequency[1], t1=2.5)
 
+
 def square_chirp(frequency, size=1000):
     return np.sign(signal.chirp(np.linspace(0, 10, size), f0=frequency[0], f1=frequency[1], t1=2.5))
+
 
 def zeros():
     return np.zeros(1000)
 
-def write_file(file, filename, fs=44100):
-    wavfile.write(dirpath+"/ICA/"+filename, fs, file)
-    
 
-def show(signals, subplot=False):
+def write_file(file, filename, fs=44100):
+    wavfile.write(dirpath+"\\"+filename, fs, file)
+
+
+def show(signals, subplot=True):
     plt.figure()
     if subplot:
-        for index in range(signals.shape[1]):
-            plt.subplot(signals.shape[1], 1, index+1)
-            plt.plot(signals[:, index])
+        if(len(signals.shape) > 1):
+            for index in range(signals.shape[1]):
+                plt.subplot(signals.shape[1], 1, index+1)
+                plt.plot(signals[:, index])
+        else:
+            plt.plot(signals)
     else:
         for index, signal in enumerate(signals):
             plt.plot(signal)
-    plt.show()
+    plt.show(block=True)
 
-    
+
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
+
+        return ret
+    return wrap
+
+def get_model(file_name):
+    with open(dirpath+'\\models\\'+file_name+'\\delays.json') as f:
+        data = json.load(f)
+
+    return data
+
+@timing
+def ICA(X):
+    print(len(X))
+    ica = FastICA(n_components=3)
+    S_ = ica.fit_transform(X)
+    A_ = ica.mixing_
+    return S_, A_
+
+
+def get_X_mat_2_sig(s1, s2, A=[[1, 1], [1, 1]], temporal_offset=1):
+    return np.c_[
+        np.add(np.dot(np.append(np.zeros(temporal_offset), s1[0:len(s1)-temporal_offset]), A[0][0]), np.dot(s2, A[0][1])),
+        np.add(np.dot(s1, A[1][0]), np.dot(np.append(np.zeros(temporal_offset), s2[0:len(s2)-temporal_offset]), A[1][1]))
+    ]
+
+
+@timing
+def getxn(model):
+    print('Fetching model : ', model)
+    model = get_model(model)
+
+    signaux = []
+
+    maxlength = -1
+
+    for source in model["sources"]:
+        if source['path'] == "basic":
+            # print('Length for', source['name'], len(get_basic_signals(source['name'], offset=int(source['t0'])*44100)))
+            length = len(get_basic_signals(source['name'], offset=int(source['t0'])*44100))
+        else:
+            # print('Length for', source['name'], len(get_entire_signal(source['path'], int(source['t0'])*44100)))
+            length = len(get_entire_signal(source['path'], int(source['t0'])*44100))
+
+        if length > maxlength:
+            maxlength = length
+
+    signaux = np.zeros(maxlength)
+
+    for source in model["sources"]:
+        # print(source)
+        # print('Max length : ', maxlength)
+        if source['path'] == "basic":
+            signal = get_basic_signals(source['name'], offset=int(source['t0'])*44100)
+            sig = np.append(signal, np.zeros(maxlength - len(signal)))
+            signaux = np.c_[signaux, sig]
+        else:
+            signal = get_entire_signal(source['path'], int(source['t0'])*44100)
+            write_file(signal, source['name'])
+            sig = np.append(signal, np.zeros(maxlength - len(signal)))
+            signaux = np.c_[signaux, sig]
+
+    signaux = np.delete(signaux,(0), axis=1)
+    # show(signaux)
+
+    for index, microphoneRelationship in enumerate(model['relationships']):
+        microphone = np.zeros(signaux.shape[0])
+        for index2, individualRelationship in enumerate(microphoneRelationship):
+            temporal_offset = round(44100*individualRelationship['delay'])
+            # print(temporal_offset, index, index2)
+            if temporal_offset > 0:
+                microphone = np.array(np.add(microphone, np.append(np.zeros(temporal_offset), np.dot(1-individualRelationship['attenuation'], signaux[:, index2])[0:-temporal_offset])))
+            else:
+                microphone = np.array(np.add(microphone, np.dot(1-individualRelationship['attenuation'], signaux[:, index2])))
+
+        if index == 0:
+            microphones = microphone
+        else:
+            microphones = np.c_[microphones, microphone]
+    return microphones
+
+
+show(getxn('triangle_2x1'))
+write_file(getxn('triangle_2x1'), "triangle_2x1.wav")
+
+def flip(X):
+    return -X
+
+
+def normalize(X):
+    return X/abs(np.amax(X))
+
+
+def calculate_error(X, Y):
+    return np.sum(np.abs(np.abs(normalize(X))-np.abs(normalize(Y))))
+
+
+def calculate_squared_error(X, Y):
+    return np.sum(np.abs(np.pow(normalize(X), 2)-np.pow(normalize(Y), 2)))
+
+
+@timing
+def pairn(S, S_):
+    signaux = []
+    for input_signal in S.T:
+        min = {"index": -1, "value": -1}
+        for index, output_signal in enumerate(S_.T):
+            if min["value"] == -1 or min["value"] > calculate_error(input_signal, output_signal):
+                min = {"index": index, "value": calculate_error(input_signal, output_signal)}
+        signaux.append(np.c_[input_signal, S_[:, min["index"]]])
+    return signaux
+
+
---
+++ python/syllabe.py
@@ -0,0 +1,52 @@
+from scipy.io import wavfile
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from pprint import pprint
+from scipy import signal
+
+
+dirpath = os.path.dirname(os.path.abspath('__file__'))
+
+def get_syllabe(data):
+    (fs, son) = wavfile.read(dirpath+'\\ICA\\G_2_personnes_2_micro_0alarme.wav')
+    return son[data[0]:data[1], 0]
+
+def get_bien(pad=False, offset=0):
+    return get_signal(60000, 20000, pad)
+
+def get_elle(pad=False, offset=0):
+    return get_signal(50000, 20000, pad)
+
+def get_lumiere(pad=False, offset=0):
+    return get_signal(75000, 20000, pad)
+
+def get_signal(offset=0, length=20000, pad=False, path='\\ICA\\G_2_personnes_2_micro_0alarme.wav'):
+    (fs, son) = wavfile.read(dirpath+'\\'+path.replace('/', '\\'))
+    if offset + 20000 > len(son): raise ValueError("The offset value must be inferior to "+str(len(son) - 20000))
+    if pad:
+        if pad > 20000: raise ValueError("The padding value must be inferior to 20000")
+        return np.append(np.zeros(pad), np.array(son[offset:offset+length-pad, 0]))/abs(np.amax(son[offset:offset+length-pad, 0]))
+    else:
+        return son[offset:offset+length, 0]/np.amax(son[offset:offset+length, 0])
+
+def get_entire_signal(path, offset):
+    (fs, son) = wavfile.read(dirpath+"\\"+path.replace('/', '\\'))
+    print('son', len(son[:, 0]))
+    print('son', son[:, 0])
+    print('sqn', len(np.append(np.zeros(offset), son[:, 0])))
+    print('sqn', np.append(np.zeros(offset), son[:, 0]))
+    sig = np.append(np.zeros(offset), son[:, 0])
+    sig = sig.astype(int)
+    return sig
+
+
+def get_basic_signals(name, offset=0, length=441000):
+    if name == "Sine wave":
+        s = np.append(np.zeros(offset), np.sin(np.linspace(0, 10, length)))
+    elif name == "Square wave":
+        s = np.append(np.zeros(offset), np.sign(np.sin(np.linspace(0, 10, length))))
+    elif name == "Sawtooth wave":
+        s = np.append(np.zeros(offset), signal.sawtooth(np.linspace(0, 10, length)))
+
+    return s
