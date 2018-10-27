6ab7abd774d09cc248da72f7cdb0c29691226c4f
Lysandre DEBUT <lysandre.debut@reseau.eseo.fr>
2018-10-27 20:02:19 +0200

    File architecture and grid layout

--- src/components/Neural/Neural.js
+++ src/components/Neural/Neural.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import '../../theme/Neural.css'
 
 export default class Neural extends React.Component{
     render(){
         return(
-            <div>
-                Neural
+            <div className="Neural">
+                <div className="Neural__Environment"></div>
+                <div className="Neural__NeuralArchitecture"></div>
+                <div className="Neural__InputSignals"></div>
+                <div className="Neural__NeuralInformation"></div>
             </div>
         )
     }
